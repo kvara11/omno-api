2a1bd4fdc34e31bb6780882341464dedb4c604dd
Nika <nikakvaratskhelia11@gmail.com>
2025-03-28 00:00:15 +0400

    add server configs

---
+++ src/configs/serverConfig.ts
@@ -0,0 +1,67 @@
+
+import { FastifyInstance } from 'fastify';
+import fastifyCors from '@fastify/cors';
+import fastifyHelmet from '@fastify/helmet';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
+
+import { addTransactionRoutes } from '../routes/transactions';
+import { webhookRoutes } from '../routes/webhooks';
+
+export default function configureServer(server: FastifyInstance) {
+
+    server.register(fastifyCors);
+    server.register(fastifyHelmet);
+    server.register(fastifySwagger, {
+        openapi: {
+            info: {
+                title: 'Omno API',
+                description: 'Omno API Documentation',
+                version: '1.0.0'
+            },
+            servers: [
+                {
+                    url: 'http://localhost'
+                }
+            ],
+            components: {
+                securitySchemes: {
+                    bearerAuth: {
+                        type: 'http',
+                        scheme: 'bearer'
+                    }
+                }
+            },
+            tags: [
+                {
+                    name: 'Root',
+                    description: 'Root endpoints'
+                }
+            ]
+        }
+    });
+
+    server.register(fastifySwaggerUi, {
+        routePrefix: '/docs',
+        uiConfig: {
+            docExpansion: 'full',
+            deepLinking: false
+        },
+        uiHooks: {
+            onRequest: function (_request, _reply, next) {
+                next();
+            },
+            preHandler: function (_request, _reply, next) {
+                next();
+            }
+        },
+        staticCSP: true,
+        transformStaticCSP: (header) => header,
+        transformSpecification: (swaggerObject) => {
+            return swaggerObject;
+        },
+        transformSpecificationClone: true
+    });
+
+    server.register(addTransactionRoutes);
+}
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,26 @@
-import Fastify from 'fastify';
-import fastifyCors from '@fastify/cors';
-import fastifyHelmet from '@fastify/helmet';
-import fastifySwagger from 'fastify-swagger';
-import { transactionRoutes } from './routes/transactions';
-import { webhookRoutes } from './routes/webhooks';
+import Fastify from'fastify';
+import 'dotenv/config';
+import configureServer from './configs/serverConfig';
 
 const server = Fastify({ logger: true });
 
-// Register Plugins
-server.register(fastifyCors);
-server.register(fastifyHelmet);
-server.register(fastifySwagger, {
-  openapi: {
-    info: { title: 'Omno API Integration', version: '1.0.0' },
-  },
-});
 
-// Register Routes
-server.register(transactionRoutes);
-server.register(webhookRoutes);
-
-// Start Server
-const start = async () => {
+async function main() {
+  
   try {
-    await server.listen({ port: 3000, host: '0.0.0.0' });
-    console.log(`Server running at http://localhost:3000`);
+    
+    configureServer(server);
+
+    const port = +(process.env.PORT || 3000);
+    await server.listen({ port, host: 'localhost' });
+
+    console.log(`Server running at http://localhost:${port}`);
+
   } catch (err) {
     server.log.error(err);
     process.exit(1);
   }
-};
+}
 
-start();
+
+main();
